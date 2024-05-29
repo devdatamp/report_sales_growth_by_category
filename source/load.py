475daba78b3dcae81c2257ea40b4d1c51f34c60c
dyer <naufaldyer@gmail.com>
2024-05-29 11:49:29 +0700

    add source loaded

--- source/load.py
+++ source/load.py
@@ -9,3 +9,8 @@ def load(df, *months):
     for month in months:
         path = f"s3://mega-dev-lake/Staging/Sales/akumulasi/growth_peritem_v2/{month}/{file_name}"
         df.to_excel(path, index=False)
+        print(f"--- Loaded to {path} ---")
+
+        path_download = f"s3://report-deliverables/sales-report/growth_peritem_v2/{month}/{file_name}"
+        df.to_excel(path_download, index=False)
+        print(f"--- Loaded to {path_download} ---")
